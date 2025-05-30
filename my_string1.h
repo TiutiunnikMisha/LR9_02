4c0883c00169109bd53387b5fa3c43dafa5b6555
TiutiunnikMisha <m.a.tiutiunnyk@student.khai.edu>
2025-05-30 22:21:15 +0300

    my_string1.h

---
+++ my_string1.h
@@ -0,0 +1,7 @@
+#ifndef MY_STRING1_H
+#define MY_STRING1_H
+
+// Прототип функції ручної реалізації методу replace()
+void my_replace(char* str, int pos, int len, const char* substr);
+
+#endif // MY_STRING1_H
